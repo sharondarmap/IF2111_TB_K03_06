26cd95387cf30dacf733d711ccc463b8664eefad
Izhrr <19623264@std.stei.itb.ac.id>
2024-11-17 16:31:10 +0700

    ADD: ADT Mesin Kata

---
+++ src/ADT/MesinKata/drivermesinkata.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "mesinkata.c"
+
+int main() {
+    printf("=== DRIVER MESIN KATA ===\n");
+
+    // 1. STARTWORD dan ADVWORD
+    printf("\n[TEST: STARTWORD dan ADVWORD]\n");
+    printf("Masukkan input (akhiri dengan '.' untuk MARK): ");
+    STARTWORD();
+
+    while (!isEndWord) {
+        printf("Kata yang dibaca: ");
+        PrintWord(currentWord); // Cetak kata yang dibaca
+        printf("\nPanjang kata: %d\n", currentWord.Length); // Tampilkan panjang kata
+        ADVWORD(); // Lanjutkan ke kata berikutnya
+    }
+    printf("=== Selesai Membaca Input ===\n");
+
+    // 2. CopyWord
+    printf("\n[TEST: CopyWord]\n");
+    printf("Masukkan input untuk CopyWord (akhiri dengan '.'): ");
+    STARTWORD();
+    printf("Kata pertama yang disalin: ");
+    PrintWord(currentWord);
+    printf("\n");
+
+    // 3. WordToInt
+    printf("\n[TEST: WordToInt]\n");
+    Word angkaWord = StringToWord("12345");
+    printf("Kata: ");
+    PrintWord(angkaWord);
+    printf("\nKonversi ke integer: %d\n", WordToInt(angkaWord));
+
+    // 4. IntToWord
+    printf("\n[TEST: IntToWord]\n");
+    int angka = 98765;
+    Word angkaKeWord = IntToWord(angka);
+    printf("Angka: %d\n", angka);
+    printf("Konversi ke Word: ");
+    PrintWord(angkaKeWord);
+    printf("\n");
+
+    // 5. IsWordEqual
+    printf("\n[TEST: IsWordEqual]\n");
+    Word word1 = StringToWord("admin");
+    Word word2 = StringToWord("admin");
+    Word word3 = StringToWord("password");
+    printf("Word1: ");
+    PrintWord(word1);
+    printf("\nWord2: ");
+    PrintWord(word2);
+    printf("\nWord3: ");
+    PrintWord(word3);
+    printf("\nWord1 == Word2: %s\n", IsWordEqual(word1, word2) ? "true" : "false");
+    printf("Word1 == Word3: %s\n", IsWordEqual(word1, word3) ? "true" : "false");
+
+    // 6. StringToWord dan WordToString
+    printf("\n[TEST: StringToWord dan WordToString]\n");
+    char str[] = "REGISTER";
+    Word strToWord = StringToWord(str);
+    char *wordToStr = WordToString(strToWord);
+    printf("String ke Word: ");
+    PrintWord(strToWord);
+    printf("\nWord ke String: %s\n", wordToStr);
+    free(wordToStr); // Bebaskan memori yang dialokasikan
+
+    return 0;
+}
---
+++ src/ADT/MesinKata/mesinkata.c
@@ -0,0 +1,156 @@
+#include <stdio.h>
+#include "mesinkata.h"
+
+boolean endWord;
+Word currentWord;
+Word currentInput;
+
+void IgnoreBlanks()
+{
+    /* Mengabaikan satu atau beberapa BLANK
+       I.S. : currentChar sembarang
+       F.S. : currentChar ≠ BLANK atau currentChar = MARK */
+    while (currentChar == BLANK && !IsEOP())
+    {
+        ADV();
+    }
+}
+
+void STARTWORD()
+{
+    /* I.S. : currentChar sembarang
+       F.S. : endWord = true, dan currentChar = MARK;
+              atau endWord = false, currentWord adalah kata yang sudah diakuisisi,
+              currentChar karakter pertama sesudah karakter terakhir kata */
+    START();
+    IgnoreBlanks();
+    if (currentChar == MARK)
+    {
+        endWord = true;
+    }
+    else
+    {
+        endWord = false;
+        CopyWord();
+    }
+}
+
+void ADVWORD()
+{
+    /* I.S. : currentChar adalah karakter pertama kata yang akan diakuisisi
+       F.S. : currentWord adalah kata terakhir yang sudah diakuisisi,
+              currentChar adalah karakter pertama dari kata berikutnya, mungkin MARK
+              Jika currentChar = MARK, endWord = true.
+       Proses : Akuisisi kata menggunakan procedure CopyWord */
+    IgnoreBlanks();
+    if (currentChar == MARK)
+    {
+        endWord = true;
+    }
+    else
+    {
+        endWord = false;
+        CopyWord();
+        IgnoreBlanks();
+    }
+}
+
+void CopyWord()
+{
+    /* Mengakuisisi kata, menyimpan dalam currentWord
+       I.S. : currentChar adalah karakter pertama dari kata
+       F.S. : currentWord berisi kata yang sudah diakuisisi;
+              currentChar = BLANK atau currentChar = MARK;
+              currentChar adalah karakter sesudah karakter terakhir yang diakuisisi.
+              Jika panjang kata melebihi CAPACITY, maka sisa kata terpotong */
+    currentWord.Length = 0;
+    while (currentChar != BLANK && currentChar != MARK)
+    {
+        if (currentWord.Length < NMax)
+        { // jika lebih akan terpotong
+            currentWord.TabWord[currentWord.Length++] = currentChar;
+            ADV();
+        }
+        else
+            break;
+    }
+}
+
+boolean isEndWord() {
+    return endWord;
+}
+
+int WordToInt(Word w) {
+    int result = 0;
+    for (int i = 0; i < w.Length; i++) {
+        result = result * 10 + (w.TabWord[i] - '0');
+    }
+    return result;
+}
+
+Word IntToWord(int num) {
+    Word w;
+    w.Length = 0;
+
+    char temp[NMax];
+    int i = 0;
+
+    if (num == 0) {
+        w.TabWord[0] = '0';
+        w.Length = 1;
+        return w;
+    }
+
+    while (num > 0) {
+        temp[i++] = '0' + (num % 10);
+        num /= 10;
+    }
+
+    for (int j = 0; j < i; j++) {
+        w.TabWord[j] = temp[i - j - 1];
+    }
+    w.Length = i;
+
+    return w;
+}
+
+Word StringToWord(char *str) {
+    Word w;
+    int i = 0;
+
+    while (str[i] != '\0' && i < NMax) {
+        w.TabWord[i] = str[i];
+        i++;
+    }
+    w.Length = i;
+
+    return w;
+}
+
+char* WordToString(Word w) {
+    char *str = malloc((w.Length + 1) * sizeof(char)); // +1 untuk null-terminator
+    for (int i = 0; i < w.Length; i++) {
+        str[i] = w.TabWord[i];
+    }
+    str[w.Length] = '\0';
+    return str;
+}
+
+void PrintWord(Word w) {
+    for (int i = 0; i < w.Length; i++) {
+        printf("%c", w.TabWord[i]);
+    }
+    printf("\n");
+}
+
+boolean IsWordEqual(Word w1, Word w2) {
+    if (w1.Length != w2.Length) {
+        return false;
+    }
+    for (int i = 0; i < w1.Length; i++) {
+        if (w1.TabWord[i] != w2.TabWord[i]) {
+            return false;
+        }
+    }
+    return true;
+}
