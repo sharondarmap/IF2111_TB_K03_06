3b6cbc9cf39374ff43ed74ed56873db41b70e659
Izhrr <19623264@std.stei.itb.ac.id>
2024-11-17 23:11:40 +0700

    ADD: Menambahkan Driver untuk MesinKarakter

---
+++ src/ADT/MesinKarakter/drivermesinkarakter.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "mesinkarakter.h"
+#include "mesinkarakter.c"
+
+int main() {
+    printf("=== DRIVER MESIN KARAKTER ===\n\n");
+
+    printf("[TEST: START]\n");
+    printf("Silakan masukkan teks berakhiran '%c' (MARK): ", MARK);
+    START(); 
+
+    printf("\n\n[TEST: ADV]\n");
+    printf("Membaca karakter satu per satu hingga MARK:\n");
+
+    while (!IsEOP()) { 
+        printf("Karakter saat ini: '%c'\n", GetCC());
+        ADV(); 
+    }
+
+    printf("\n[TEST: IsEOP]\n");
+    if (IsEOP()) {
+        printf("Akhir pita (MARK) telah tercapai.\n");
+    }
+
+    printf("\nDriver selesai dijalankan.\n");
+
+    return 0;
+}
