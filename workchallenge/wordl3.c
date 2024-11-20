5c910d8c624ee2a2b94eb4dd0eb92bf144e56a25
sharondarmap <19623106@std.stei.itb.ac.id>
2024-11-20 21:49:18 +0700

    feat: new function TandaTebakan and DisplayGrid allowing to change the way of printing the answers with correct gridding

--- workchallenge/wordl3.c
+++ workchallenge/wordl3.c
@@ -5,20 +5,38 @@
 #include "../src/ADT/MesinKata/mesinkata.h"
 /* Library yang boleh digunakan hanya stdio.h, stdlib.h, time.h, dan math.h. */
 
-/* typedef struct
-{
-   char TabWord[NMax]; container penyimpan kata, indeks yang dipakai [0..NMax-1]
-   int Length;
-   } Word  
-*/
-
-boolean CharExist(char c, Word *wordAns){
-    for (int i = 0; i < wordAns->Length; i++){
-        if (c == wordAns->TabWord[i]){
+boolean CharExist(char c, Word *wordAns) {
+    for (int i = 0; i < wordAns->Length; i++) {
+        if (c == wordAns->TabWord[i]) {
             return true;
         }
     }
     return false;
+}
+
+void TandaTebakan(char *guess, Word *wordAns, char grid[5][6], int attempt, char answer[6]) {
+    // Change answer to char[6], not a single character
+    for (int i = 0; i < 5; i++) {
+        if (guess[i] == answer[i]) {
+            grid[attempt][i] = guess[i]; // Correct letter in the correct position
+        } else if (CharExist(guess[i], wordAns)) {
+            grid[attempt][i] = '#'; // Correct letter in the wrong position
+        } else {
+            grid[attempt][i] = '*'; // Letter not in the answer
+        }
+    }
+    grid[attempt][5] = '\0'; // Null-terminate the row for printing
+}
+
+void DisplayGrid(char grid[5][6]) {
+    printf("\n");
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            printf("%c ", grid[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
 }
 
 void WORDL3(){
@@ -27,21 +45,21 @@ void WORDL3(){
     char answer[6]; //container jawaban string
 
     char solution[100][6] = {
-    "actor", "amber", "angel", "apple", "baker", "beach", 
-    "blend", "brisk", "candy", "charm", "clear", "crane", 
-    "dense", "delta", "dodge", "dream", "eager", "eagle", 
-    "elder", "ethic", "fable", "flame", "flute", "frost", 
-    "giant", "glare", "globe", "grain", "heist", "honey", 
-    "house", "hover", "image", "index", "input", "ivory", 
-    "jewel", "joker", "jolly", "jumpy", "kinky", "knife", 
-    "knock", "kneel", "layer", "latch", "lemon", "lofty", 
-    "mango", "march", "mirth", "nylon", "noble", "north", 
-    "ocean", "oasis", "orbit", "pearl", "pivot", "plant", 
-    "queen", "query", "quake", "raven", "rider", "rocky", 
-    "snake", "silly", "stone", "tears", "toast", "trump", 
-    "ultra", "union", "urban", "valve", "vivid", "vocal", 
-    "wagon", "whale", "witty", "xenon", "xerox", "xylos", 
-    "yacht", "yummy", "youth", "zebra", "zesty", "zonal"
+        "actor", "amber", "angel", "apple", "baker", "beach", 
+        "blend", "brisk", "candy", "charm", "clear", "crane", 
+        "dense", "delta", "dodge", "dream", "eager", "eagle", 
+        "elder", "ethic", "fable", "flame", "flute", "frost", 
+        "giant", "glare", "globe", "grain", "heist", "honey", 
+        "house", "hover", "image", "index", "input", "ivory", 
+        "jewel", "joker", "jolly", "jumpy", "kinky", "knife", 
+        "knock", "kneel", "layer", "latch", "lemon", "lofty", 
+        "mango", "march", "mirth", "nylon", "noble", "north", 
+        "ocean", "oasis", "orbit", "pearl", "pivot", "plant", 
+        "queen", "query", "quake", "raven", "rider", "rocky", 
+        "snake", "silly", "stone", "tears", "toast", "trump", 
+        "ultra", "union", "urban", "valve", "vivid", "vocal", 
+        "wagon", "whale", "witty", "xenon", "xerox", "xylos", 
+        "yacht", "yummy", "youth", "zebra", "zesty", "zonal"
     };
 
     for (int i = 0; i < 5; i++) {
@@ -49,8 +67,7 @@ void WORDL3(){
     }
     answer[5] = '\0';  // Null-terminate the string
 
-    Word wordAns = StringToWord(answer); //ubah string menjadi word
-    char coba = wordAns.TabWord[0]; //ngambil huruf pertama dari Word kek gini ya bang
+    Word wordAns = StringToWord(answer); // Convert string to Word struct
 
     printf("WELCOME TO W0RDL3, YOU HAVE 5 CHANCES TO ANSWER BEFORE YOU LOSE!\n");
     printf(" \n");
@@ -61,29 +78,51 @@ void WORDL3(){
             "4. # menunjukkan huruf ada di dalam jawaban tetapi posisi salah\n"
             "5. Jika huruf benar dan posisi benar, huruf akan dikembalikan tanpa tanda\n");
 
-    printf("_ _ _ _ _\n"
+    /*printf("_ _ _ _ _\n"
         "_ _ _ _ _\n"
         "_ _ _ _ _\n"
         "_ _ _ _ _\n"
-        "_ _ _ _ _\n");
+        "_ _ _ _ _\n");*/
 
-    printf("%c\n", coba);
-    
-    while (chances < 5){
+    char grid[5][6] = {
+        "_____",
+        "_____",
+        "_____",
+        "_____",
+        "_____"
+    };
+
+    while (chances < 5) {
+        DisplayGrid(grid);
+
         printf("Masukkan kata tebakan Anda: ");
-        STARTWORD();
+        STARTWORD(); // Get the user's guess
+        char guess[6];
+        for (int i = 0; i < currentWord.Length; i++) {
+            guess[i] = currentWord.TabWord[i];
+        }
+        guess[currentWord.Length] = '\0'; // Null-terminate the guess
 
+        if (currentWord.Length != 5) {
+            printf("Invalid guess. Please enter exactly 5 letters.\n");
+            continue;
+        }
 
+        // Provide feedback and update the grid
+        TandaTebakan(guess, &wordAns, grid, chances, answer);
 
+        if (IsWordEqual(currentWord, wordAns)) {
+            printf("Congratulations! You've guessed the word: %s\n", answer);
+            return;
+        }
 
-
+        chances++;
     }
-
-    printf("%s\n", answer); //ini buat ngeter jawaban aja sie, nanti diapus
+    DisplayGrid(grid);
+    printf("You've run out of chances! The correct word was: %s\n", answer);
 }
 
 int main(){
     WORDL3();
-
     return 0;
 }
