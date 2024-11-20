439aa34114c14478b14c6c25f7a88901b9ff0fd8
sharondarmap <19623106@std.stei.itb.ac.id>
2024-11-20 12:35:47 +0700

    feat: Wordle function opening

--- workchallenge/wordl3.c
+++ workchallenge/wordl3.c
@@ -1,1 +1,30 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include "../src/ADT/Kustom/user.h"
+#include "../src/ADT/MesinKata/mesinkata.h"
+/* Library yang boleh digunakan hanya stdio.h, stdlib.h, time.h, dan math.h. */
+
+void WORDL3(){
+    int chances = 0;
+
+    printf("WELCOME TO W0RDL3, YOU HAVE 5 CHANCES TO ANSWER BEFORE YOU LOSE!\n");
+    printf("_ _ _ _ _\n"
+        "_ _ _ _ _\n"
+        "_ _ _ _ _\n"
+        "_ _ _ _ _\n"
+        "_ _ _ _ _\n");
+
+    while (chances < 5){
+        
+    }
+    
+    
+
+}
+
+int main(){
+    WORDL3();
+
+    return 0;
+}
