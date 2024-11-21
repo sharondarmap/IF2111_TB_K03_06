b771e6a920526940e0961b08968067c841bd546b
sharondarmap <19623106@std.stei.itb.ac.id>
2024-11-21 14:02:00 +0700

    feat: adding worl3.h and tebakangka.h

---
+++ workchallenge/wordl3.h
@@ -0,0 +1,19 @@
+#ifndef WORDL3_H
+#define WORDL3_H
+
+#include "../src/ADT/Kustom/user.h"
+#include "../src/ADT/MesinKata/mesinkata.h"
+
+// checks if a character exists in a Word
+boolean CharExist(char c, Word *wordAns);
+
+// tanda-tanda untuk tebakan huruf
+void TandaTebakan(char *guess, Word *wordAns, char grid[5][6], int attempt, char answer[6]);
+
+//display grid pada game
+void DisplayGrid(char grid[5][6]);
+
+//main game wordl3
+int WORDL3();
+
+#endif // WORDL3_H
