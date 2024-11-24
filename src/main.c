ecbd2cfff84fb206348e06ad05396dcdd985c649
Izhrr <19623264@std.stei.itb.ac.id>
2024-11-24 22:06:18 +0700

    FIX: Penyesuaian command  pada fungsi main

--- src/main.c
+++ src/main.c
@@ -154,17 +154,8 @@ int main()
                 else if (IsWordEqual(currentWord, StringToWord("REQUEST")))
                 {
                     ADVWORD();
-                    if (IsWordEqual(currentWord, StringToWord("BIO")))
-                    {
-                        ADVWORD();
-                        if (IsWordEqual(currentWord, StringToWord("WEAPON")))
-                        {
-                            StoreRequestBioWeapon(&barangQueue, &barangList);
-                        }
-                        else
-                        {
-                            printf("Command tidak dikenal.\n");
-                        }
+                    if (IsWordEqual(currentWord, StringToWord("BIOWEAPON"))){
+                        StoreRequestBioWeapon(&barangQueue, &barangList);
                     }
                     else if (endWord)
                     {
@@ -186,10 +177,6 @@ int main()
                 {
                     printf("Command tidak dikenal.\n");
                 }
-            }
-            else if (IsWordEqual(currentWord, StringToWord("BIOWEAPON")))
-            {
-                StoreRequestBioWeapon(&barangQueue, &barangList);
             }
             else if (IsWordEqual(currentWord, StringToWord("LOGOUT")))
             {
