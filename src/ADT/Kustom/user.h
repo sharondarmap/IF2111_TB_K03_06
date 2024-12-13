935794f42597d4c00fecc74e6f2955c743bc4dbe
sharondarmap <19623106@std.stei.itb.ac.id>
2024-12-13 21:34:55 +0700

    feat: modifying user.h

--- src/ADT/Kustom/user.h
+++ src/ADT/Kustom/user.h
@@ -4,10 +4,15 @@
 /* Kamus Umum */
 #define MAX_LEN 100
 
+#include "../Stack/stack.h"
+
 typedef struct {
   char name[MAX_LEN];
   char password[MAX_LEN];
   int money;
+  Map keranjang;
+  Stack riwayat_pembelian;
+  Linkedlist wishlist;
 } User;
 
 #endif
