70b2c362ffd138d8739712ee0f93fd817bace1ed
Nakeisha Valya <167664928+NakeishaValya@users.noreply.github.com>
2024-12-18 06:47:14 +0700

    Create WishlistSwap.h
    feat : menambahkan header WishlistSwap

---
+++ src/SPESIFIKASI/WishlistSwap/WishlistSwap.h
@@ -0,0 +1,14 @@
+#ifndef WISHLIST_SWAP_H
+#define WISHLIST_SWAP_H
+
+#include <stdio.h>
+#include "../../boolean.h"
+#include "../../ADT/MesinKata/mesinkata.h"
+#include "../../ADT/Linkedlist/linkedlist.h"
+
+void WishlistSwap(Linkedlist *wishlist, int i, int j);
+/*
+I.S.: Wishlist berisi barang dengan posisi yang ditentukan pengguna, dan posisi i serta j valid.
+F.S.: Wishlist terupdate dengan posisi barang pada i dan j yang telah ditukar.
+*/
+#endif
