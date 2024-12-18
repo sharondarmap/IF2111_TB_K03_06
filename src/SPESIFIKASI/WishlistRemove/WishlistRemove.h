3a83e51bc16b5f45d91a0b273d21c6a87123e71d
Nakeisha Valya <167664928+NakeishaValya@users.noreply.github.com>
2024-12-18 19:49:53 +0700

    Feat : Menambahkan Fitur WishlistRemove

---
+++ src/SPESIFIKASI/WishlistRemove/WishlistRemove.h
@@ -0,0 +1,15 @@
+#ifndef WISHLIST_REMOVE_H
+#define WISHLIST_REMOVE_H
+
+#include <stdio.h>
+#include "../../boolean.h"
+#include "../../ADT/Linkedlist/linkedlist.h"
+#include "../../ADT/MesinKata/mesinkata.h"
+#include "../../ADT/MesinKalimat/mesinkalimat.h"
+
+void WishlistRemove(Linkedlist *L, Word inputName);
+/*
+I.S.: Wishlist berisi barang dengan nama yang dicari pengguna, atau nama tidak ada dalam wishlist.
+F.S.: Wishlist terupdate dengan barang yang memiliki nama sesuai input pengguna telah dihapus, atau tidak ada perubahan jika barang tidak ditemukan.
+*/
+#endif
