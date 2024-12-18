cb749ab0cbd03947ba4aa9a3af67adcf341b49e4
Nakeisha Valya <167664928+NakeishaValya@users.noreply.github.com>
2024-12-18 19:52:10 +0700

    Feat : Menambahkan Fitur WishlistRemoveIdx

---
+++ src/SPESIFIKASI/WishlistRemoveIdx/WishlistRemoveIdx.h
@@ -0,0 +1,14 @@
+#ifndef WISHLIST_REMOVE_IDX_H
+#define WISHLIST_REMOVE_IDX_H
+
+#include <stdio.h>
+#include "../../boolean.h"
+#include "../../ADT/MesinKata/mesinkata.h"
+#include "../../ADT/Linkedlist/linkedlist.h"
+
+void WishlistRemoveIdx(Linkedlist *L, int i);
+/*
+I.S.: Wishlist berisi barang dengan posisi yang valid, atau posisi tidak valid.
+F.S.: Wishlist terupdate dengan barang pada posisi yang diberikan telah dihapus, atau tidak ada perubahan jika posisi tidak valid.
+*/
+#endif 
